aa44fa9a175b0549718bb51df2969ed80c6a57ad
Dereje <dereje2012k@gmail.com>
2025-06-20 18:34:31 +0300

    Complete all buttons functionality
    add functionalities of remove button, read button and new book button.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let exist = [];
 
 function Book(title, author, pages, read) {
   if (!new.target) {
@@ -11,24 +12,81 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  // const book = new Book(title, author, pages, read);
+Book.prototype.toggleReadStatus = function () {
+  const readBtn = cards.querySelector(`[data-id="${this.id}"].read`);
 
+  if (readBtn.textContent === "Read") {
+    readBtn.textContent = "Not Read";
+    myLibrary.forEach((book) => {
+      if (book.id === this.id) {
+        book.read = false;
+      }
+    });
+  } else if (readBtn.textContent === "Not Read") {
+    readBtn.textContent = "Read";
+    myLibrary.forEach((book) => {
+      if (book.id === this.id) {
+        book.read = true;
+      }
+    });
+  }
+};
+
+function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
+  displayBooks();
 }
 
-addBookToLibrary("m", "M", 2, false);
-addBookToLibrary("me", "ME", 23, true);
-addBookToLibrary("mee", "MEe", 233, false);
-
-const cards = document.querySelector(".cards");
-const dialog = document.querySelector("dialog");
-const cancelDialog = document.querySelector("dialog .cancel");
 const newBookBtn = document.querySelector("header .new-book");
+let cards = document.querySelector(".cards");
+const dialog = document.querySelector("dialog");
+const cancel = document.querySelector("dialog .cancel");
+const discard = document.querySelector("dialog .discard");
+const save = document.querySelector("dialog .save");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readInput = document.querySelector("#read");
+
+// books display example. use 'New Book' button to create more book display on the page.
+addBookToLibrary("Demo title", "Demo author", 1, true);
+displayBooks();
+setEventListeners();
+
+function setEventListeners() {
+  const removeBtns = document.querySelectorAll(".remove");
+  const readStatusBtns = document.querySelectorAll(".read");
+  removeBtns.forEach((removeBtn) => {
+    removeBtn.addEventListener("click", () => {
+      const id = removeBtn.dataset.id;
+      removeCard(id);
+    });
+  });
+
+  readStatusBtns.forEach((readStatusBtn) => {
+    readStatusBtn.addEventListener("click", () => {
+      const id = readStatusBtn.dataset.id;
+      myLibrary.forEach((book) => {
+        if (book.id === id) {
+          book.toggleReadStatus();
+        }
+      });
+    });
+  });
+}
 
 function displayBooks() {
-  myLibrary.forEach((item) => {
-    cards.innerHTML += `
+  const i = document.createElement("i");
+  i.textContent =
+    "It seems like our library is empty. Use 'New Book' button to create more books on the page.";
+
+  if (myLibrary.length >= 1) {
+    myLibrary.forEach((book) => {
+      if (!exist.includes(book.id)) {
+        if (cards.querySelector("i")) {
+          cards.innerHTML = "";
+        }
+        cards.innerHTML += `
       <div>
         <section>
           <div>
@@ -39,22 +97,68 @@ function displayBooks() {
             />
           </div>
           <div class="book-info">
-            <p>Title: ${item.title}</p>
-            <p>Author: ${item.author}</p>
-            <p>Pages: ${item.pages}</p>
+            <p>Title: ${book.title}</p>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
           </div>
         </section>
-        <button class="remove btn">Remove</button>
-        <button class="read btn">${
-          item.read === true ? "Read" : "Not Read"
+        <button class="remove btn" data-id="${book.id}">Remove</button>
+        <button class="read btn" data-id="${book.id}">${
+          book.read === true ? "Read" : "Not Read"
         }</button>
       </div>
     `;
-  });
+        exist.push(book.id);
+      }
+    });
+  } else {
+    cards.appendChild(i);
+  }
 }
 
-displayBooks();
+function resetInputs() {
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readInput.value = "";
+}
 
 newBookBtn.addEventListener("click", () => {
   dialog.showModal();
 });
+
+cancel.addEventListener("click", () => {
+  dialog.close();
+});
+
+discard.addEventListener("click", () => {
+  resetInputs();
+  dialog.close();
+});
+
+save.addEventListener("click", () => {
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
+  const read = readInput.value.toLowerCase() === "yes" ? true : false;
+
+  if (title !== "" && author !== "" && pages !== "" && read !== "") {
+    addBookToLibrary(title, author, pages, read);
+    setEventListeners();
+  } else {
+    alert("Please fill in all fields.");
+  }
+});
+
+function removeCard(id) {
+  const index = myLibrary.findIndex((book) => book.id === id);
+  if (index !== -1) {
+    myLibrary.splice(index, 1);
+    cards.innerHTML = "";
+    exist = [];
+    displayBooks();
+    setEventListeners();
+  } else {
+    console.log("can't find object to be deleted");
+  }
+}
